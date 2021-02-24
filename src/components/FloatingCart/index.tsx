2427bc14fec9b5f8764d8c7ea38c8d937d786714
guilhermeabel99 <gpeuvis@gmail.com>
2021-02-24 15:13:58 -0300

    challenge finalized

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useMemo } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import FeatherIcon from 'react-native-vector-icons/Feather';
+
 import {
   Container,
   CartPricing,
